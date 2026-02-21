86ab4763d1a0c6eea1d304868b6191a0622254b3
ghost <impulseontopnostand@gmail.com>
2026-02-21 11:53:42 +0100

    Favicon Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Cherax Wiki',
+			favicon: '/logo.png',
 			social: [],
 			customCss: ['./src/assets/video-embed.css'],
 			components: {
